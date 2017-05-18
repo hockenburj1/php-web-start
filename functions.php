cfcf5a32e87eacd15786bb42441823664180d645
Jesse Hockenbury <hockenburj1@nku17r7051.hh.nku.edu>
2017-05-18 16:02:10 -0400

    added file upload functionality

--- functions.php
+++ functions.php
@@ -46,7 +46,7 @@ function data($var) {
 }
 
 function get_sql($sqlName) {
-    return file_get_contents("sql/$sqlName.sql");
+    return file_get_contents(ROOT . "sql/$sqlName.sql");
 }
 
 function snippet($string, $characters) {
@@ -57,4 +57,54 @@ function snippet($string, $characters) {
     $string = substr($string, 0, $characters);
     $string .= '...';
     return $string;
+}
+
+function file_upload($file, $target_directory, $filename, $type) {
+    $target_dir = $target_directory;
+    $target_file = $target_dir . $filename;
+    $uploadOk = 1;
+    $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    echo "file type: " . $fileType;
+    
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+    
+    // Check file size
+    if ($file["size"] > 5000000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    
+    switch($type) {
+        case 'image':
+            $allowedExtensions = array('jpg', 'jpeg', 'png', 'gif');
+            $error = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            break; 
+        default:
+            $allowedExtensions = array('jpg', 'jpeg', 'png', 'gif');
+            $error = "Sorry, type not supported";
+    }
+    
+    // Allow certain file formats
+    if(!in_array($fileType, $allowedExtensions)) {
+        echo $error;
+        $uploadOk = 0;
+    }
+    
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($file["tmp_name"], $target_file)) {
+            echo "The file ". basename( $file["name"]). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+    
+    return $uploadOk;
 }
